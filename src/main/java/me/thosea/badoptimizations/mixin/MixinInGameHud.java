ae72323ae4a4e7779b8a085ff26d3f713e1757d8
Thosea / JF <104597976+ItsThosea@users.noreply.github.com>
2023-12-15 19:58:49 -0500

    1.2.0
    (cherry picked from commit 2c1b67a48818a78a2594d37ba0dc51679d9fc232)

--- src/main/java/me/thosea/badoptimizations/mixin/MixinInGameHud.java
+++ src/main/java/me/thosea/badoptimizations/mixin/MixinInGameHud.java
@@ -1,5 +1,8 @@
 package me.thosea.badoptimizations.mixin;
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.hud.InGameHud;
@@ -9,7 +12,6 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(InGameHud.class)
@@ -17,9 +19,9 @@ public final class MixinInGameHud {
 	@Shadow @Final private MinecraftClient client;
 	@Shadow private float spyglassScale;
 
-	@Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/MathHelper;lerp(FFF)F", ordinal = 0))
-	private float calculateSpyglass(float delta, float start, float end) {
-		return spyglassScale;
+	@ModifyExpressionValue(method = "render", at = @At(value = "FIELD", target = "Lnet/minecraft/client/gui/hud/InGameHud;spyglassScale:F", ordinal = 0))
+	private float calculateSpyglass(float original) {
+		return spyglassScale; // Discard lerp result
 	}
 
 	@Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/InGameHud;renderSpyglassOverlay(Lnet/minecraft/client/gui/DrawContext;F)V"))
@@ -27,12 +29,13 @@ public final class MixinInGameHud {
 		spyglassScale = MathHelper.lerp(0.5F * client.getLastFrameDuration(), spyglassScale, 1.125F);
 	}
 
-	@Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/MathHelper;lerp(FFF)F", ordinal = 1))
-	private float calculatePortalEffect(float delta, float start, float end) {
-		if(!client.player.inNetherPortal)
+	@WrapOperation(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/MathHelper;lerp(FFF)F", ordinal = 1))
+	private float calculatePortalEffect(float delta, float start, float end, Operation<Float> original) {
+		if(!client.player.inNetherPortal) {
 			return 0f;
-
-		return MathHelper.lerp(delta, start, end);
+		} else {
+			return original.call(delta, start, end);
+		}
 	}
 
 	@Inject(method = "tickAutosaveIndicator", at = @At("HEAD"), cancellable = true)
--- src/main/java/me/thosea/badoptimizations/mixin/entitydata/MixinLivingEntity.java
+++ src/main/java/me/thosea/badoptimizations/mixin/entitydata/MixinLivingEntity.java
@@ -1,28 +1,24 @@
 package me.thosea.badoptimizations.mixin.entitydata;
 
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.data.DataTracker;
 import net.minecraft.entity.data.TrackedData;
 import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
-import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.Optional;
 
 @Mixin(LivingEntity.class)
-public abstract class MixinLivingEntity extends Entity {
+public abstract class MixinLivingEntity extends MixinEntity {
 	@Shadow @Final protected static TrackedData<Byte> LIVING_FLAGS;
 	@Unique private boolean isUsingItem = false;
 	@Unique private Hand activeHand = Hand.MAIN_HAND;
@@ -46,24 +42,24 @@ public abstract class MixinLivingEntity extends Entity {
 	@Shadow @Final private static TrackedData<Optional<BlockPos>> SLEEPING_POSITION;
 	@Unique private Optional<BlockPos> sleepingPosition = Optional.empty();
 
-	@Overwrite
-	public boolean isUsingItem() {
-		return isUsingItem;
+	@Inject(method = "isUsingItem", at = @At("HEAD"), cancellable = true)
+	private void isUsingItem(CallbackInfoReturnable<Boolean> cir) {
+		cir.setReturnValue(isUsingItem);
 	}
 
-	@Overwrite
-	public Hand getActiveHand() {
-		return activeHand;
+	@Inject(method = "getActiveHand", at = @At("HEAD"), cancellable = true)
+	private void getActiveHand(CallbackInfoReturnable<Hand> cir) {
+		cir.setReturnValue(activeHand);
 	}
 
-	@Overwrite
-	public boolean isUsingRiptide() {
-		return isUsingRiptide;
+	@Inject(method = "isUsingRiptide", at = @At("HEAD"), cancellable = true)
+	private void isUsingRiptide(CallbackInfoReturnable<Boolean> cir) {
+		cir.setReturnValue(isUsingRiptide);
 	}
 
-	@Overwrite
-	public float getHealth() {
-		return health;
+	@Inject(method = "getHealth", at = @At("HEAD"), cancellable = true)
+	private void getHealth(CallbackInfoReturnable<Float> cir) {
+		cir.setReturnValue(health);
 	}
 
 	@Redirect(method = "tickStatusEffects", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/data/DataTracker;get(Lnet/minecraft/entity/data/TrackedData;)Ljava/lang/Object;", ordinal = 0))
@@ -76,50 +72,43 @@ public abstract class MixinLivingEntity extends Entity {
 		return potionSwirlsAmbient;
 	}
 
-	@Overwrite
-	public final int getStuckArrowCount() {
-		return stuckArrowCount;
+	@Inject(method = "getStuckArrowCount", at = @At("HEAD"), cancellable = true)
+	private void getStuckArrowCount(CallbackInfoReturnable<Integer> cir) {
+		cir.setReturnValue(stuckArrowCount);
 	}
 
-	@Overwrite
-	public final int getStingerCount() {
-		return stingerCount;
+	@Inject(method = "getStingerCount", at = @At("HEAD"), cancellable = true)
+	private void getStingerCount(CallbackInfoReturnable<Integer> cir) {
+		cir.setReturnValue(stingerCount);
 	}
 
-	@Overwrite
-	public Optional<BlockPos> getSleepingPosition() {
-		return sleepingPosition;
+	@Inject(method = "getSleepingPosition", at = @At("HEAD"), cancellable = true)
+	private void getSleepingPosition(CallbackInfoReturnable<Optional<BlockPos>> cir) {
+		cir.setReturnValue(sleepingPosition);
 	}
 
-	@Redirect(method = "tickRiptide", at = @At(value = "FIELD", target = "Lnet/minecraft/world/World;isClient:Z", opcode = Opcodes.GETFIELD))
-	private boolean onRiptideEnd(World instance) {
-		return false;
-	}
+	@Override
+	protected void onDataSet(TrackedData<?> data, CallbackInfo ci) {
+		super.onDataSet(data, ci);
 
-	@Inject(method = "onTrackedDataSet", at = @At("HEAD"))
-	private void onDataSet(TrackedData<?> data, CallbackInfo ci) {
-		DataTracker ourDataTracker = getDataTracker();
+		DataTracker dataTracker = getDataTracker();
 
 		if(LIVING_FLAGS.equals(data)) {
-			isUsingItem = (ourDataTracker.get(LIVING_FLAGS) & 1) > 0;
-			activeHand = (ourDataTracker.get(LIVING_FLAGS) & 2) > 0 ? Hand.OFF_HAND : Hand.MAIN_HAND;
-			isUsingRiptide = (ourDataTracker.get(LIVING_FLAGS) & 4) != 0;
+			isUsingItem = (dataTracker.get(LIVING_FLAGS) & 1) > 0;
+			activeHand = (dataTracker.get(LIVING_FLAGS) & 2) > 0 ? Hand.OFF_HAND : Hand.MAIN_HAND;
+			isUsingRiptide = (dataTracker.get(LIVING_FLAGS) & 4) != 0;
 		} else if(HEALTH.equals(data)) {
-			health = ourDataTracker.get(HEALTH);
+			health = dataTracker.get(HEALTH);
 		} else if(POTION_SWIRLS_COLOR.equals(data)) {
-			potionSwirlsColor = ourDataTracker.get(POTION_SWIRLS_COLOR);
+			potionSwirlsColor = dataTracker.get(POTION_SWIRLS_COLOR);
 		} else if(POTION_SWIRLS_AMBIENT.equals(data)) {
-			potionSwirlsAmbient = ourDataTracker.get(POTION_SWIRLS_AMBIENT);
+			potionSwirlsAmbient = dataTracker.get(POTION_SWIRLS_AMBIENT);
 		} else if(STUCK_ARROW_COUNT.equals(data)) {
-			stuckArrowCount = ourDataTracker.get(STUCK_ARROW_COUNT);
+			stuckArrowCount = dataTracker.get(STUCK_ARROW_COUNT);
 		} else if(STINGER_COUNT.equals(data)) {
-			stingerCount = ourDataTracker.get(STINGER_COUNT);
+			stingerCount = dataTracker.get(STINGER_COUNT);
 		} else if(SLEEPING_POSITION.equals(data)) {
-			sleepingPosition = ourDataTracker.get(SLEEPING_POSITION);
+			sleepingPosition = dataTracker.get(SLEEPING_POSITION);
 		}
-	}
-
-	protected MixinLivingEntity(EntityType<? extends LivingEntity> entityType, World world) {
-		super(entityType, world);
 	}
 }
