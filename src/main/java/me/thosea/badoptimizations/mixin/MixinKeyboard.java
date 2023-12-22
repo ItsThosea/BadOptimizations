3c583682ebbfa1718088b15500e202dfbca1641e
Thosea / JF <104597976+ItsThosea@users.noreply.github.com>
2023-12-22 01:33:16 -0500

    1.3.0

---
+++ src/main/java/me/thosea/badoptimizations/mixin/MixinKeyboard.java
@@ -0,0 +1,21 @@
+package me.thosea.badoptimizations.mixin;
+
+import me.thosea.badoptimizations.ClientAccessor;
+import net.minecraft.client.Keyboard;
+import net.minecraft.client.MinecraftClient;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Keyboard.class)
+public final class MixinKeyboard {
+	@Shadow @Final private MinecraftClient client;
+
+	@Inject(method = "onKey", at = @At(value = "FIELD", ordinal = 0, target = "Lnet/minecraft/client/option/GameOptions;hudHidden:Z"))
+	private void onToggleHud(long window, int key, int scancode, int action, int modifiers, CallbackInfo ci) {
+		((ClientAccessor) client).updateFpsString();
+	}
+}
