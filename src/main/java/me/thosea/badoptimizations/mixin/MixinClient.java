ca9da152d496c4805cc6ab6441492199ffe98e6a
Thosea / JF <104597976+ItsThosea@users.noreply.github.com>
2023-12-22 02:21:22 -0500

    1.3.0

--- src/main/java/me/thosea/badoptimizations/mixin/MixinClient.java
+++ src/main/java/me/thosea/badoptimizations/mixin/MixinClient.java
@@ -15,8 +15,6 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.util.Locale;
-
 @Mixin(MinecraftClient.class)
 public abstract class MixinClient implements ClientAccessor {
 	@Shadow private static int currentFps;
@@ -27,47 +25,66 @@ public abstract class MixinClient implements ClientAccessor {
 	@Shadow public String fpsDebugString;
 	@Shadow private double gpuUtilizationPercentage;
 
-	@Shadow protected abstract int getFramerateLimit();
+	@Shadow
+	protected abstract int getFramerateLimit();
 
 	@Shadow private @Nullable GlTimer.Query currentGlTimerQuery;
 	@Shadow private long metricsSampleDuration;
 
 	@Override
 	public void updateFpsString() {
-		// decompiled
-		int k = getFramerateLimit();
+		int limit = getFramerateLimit();
 
 		if(currentGlTimerQuery != null && currentGlTimerQuery.isResultAvailable()) {
 			this.gpuUtilizationPercentage = (double) this.currentGlTimerQuery.queryResult() * 100.0 / (double) metricsSampleDuration;
 		}
 
-		String string;
-		if(gpuUtilizationPercentage > 0.0) {
-			String var10000 = gpuUtilizationPercentage > 100.0 ? Formatting.RED + "100%" : Math.round(this.gpuUtilizationPercentage) + "%";
-			string = " GPU: " + var10000;
-		} else {
-			string = "";
+		StringBuilder builder = new StringBuilder(32);
+
+		builder.append(currentFps).append(" fps T: ");
+		builder.append(limit == 260 ? "inf" : limit);
+
+		if(this.options.getEnableVsync().getValue()) {
+			builder.append(" vsync");
 		}
 
-		this.fpsDebugString = String.format(Locale.ROOT, "%d fps T: %s%s%s%s B: %d%s", currentFps, k == 260 ? "inf" : k, this.options.getEnableVsync()
-				.getValue() ? " vsync" : "", this.options.getGraphicsMode()
-				.getValue(), this.options.getCloudRenderMode()
-				.getValue() == CloudRenderMode.OFF ? "" : (this.options.getCloudRenderMode()
-				.getValue() == CloudRenderMode.FAST ? " fast-clouds" : " fancy-clouds"), this.options.getBiomeBlendRadius()
-				.getValue(), string);
+		builder.append(options.getGraphicsMode().getValue());
+
+		CloudRenderMode clouds = options.getCloudRenderMode().getValue();
+		if(clouds != CloudRenderMode.OFF) {
+			builder.append(clouds == CloudRenderMode.FAST ? " fast-clouds" : " fancy-clouds");
+		}
+
+		builder.append(" B: ");
+		builder.append(this.options.getBiomeBlendRadius().getValue());
+
+		if(gpuUtilizationPercentage > 0.0) {
+			String var10000 = gpuUtilizationPercentage > 100.0 ? Formatting.RED + "100%" : Math.round(this.gpuUtilizationPercentage) + "%";
+			builder.append(" GPU: ").append(var10000);
+		}
+
+		// this.fpsDebugString = String.format(Locale.ROOT, "%d fps T: %s%s%s%s B: %d%s", currentFps, k == 260 ? "inf" : k, this.options.getEnableVsync()
+		// 		.getValue() ? " vsync" : "", this.options.getGraphicsMode()
+		// 		.getValue(), this.options.getCloudRenderMode()
+		// 		.getValue() == CloudRenderMode.OFF ? "" : (this.options.getCloudRenderMode()
+		// 		.getValue() == CloudRenderMode.FAST ? " fast-clouds" : " fancy-clouds"), this.options.getBiomeBlendRadius()
+		// 		.getValue(), string);
+		this.fpsDebugString = builder.toString();
 	}
 
 	@Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/profiler/Profiler;push(Ljava/lang/String;)V", ordinal = 7), cancellable = true)
 	private void onDebugHandle(boolean tick, CallbackInfo ci) {
-		if(!options.debugEnabled) {
-			ci.cancel();
+		ci.cancel();
 
-			if(Util.getMeasuringTimeMs() >= nextDebugInfoUpdateTime + 1000L) {
-				// Track FPS
-				currentFps = fpsCounter;
-				nextDebugInfoUpdateTime += 1000L;
-				fpsCounter = 0;
-				fpsDebugString = "";
+		if(Util.getMeasuringTimeMs() >= nextDebugInfoUpdateTime + 1000L) {
+			// Track FPS
+			currentFps = fpsCounter;
+			nextDebugInfoUpdateTime += 1000L;
+			fpsCounter = 0;
+			fpsDebugString = "";
+
+			if(options.debugEnabled) {
+				updateFpsString();
 			}
 		}
 	}
