410ca3d1b0dc399e7945d1e788681cad284845ff
Thosea <104597976+ItsThosea@users.noreply.github.com>
2023-12-23 23:57:02 -0500

    1.3.1

--- src/main/java/me/thosea/badoptimizations/mixin/MixinKeyboard.java
+++ src/main/java/me/thosea/badoptimizations/mixin/MixinKeyboard.java
@@ -7,6 +7,7 @@ import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.At.Shift;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
@@ -14,8 +15,10 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public final class MixinKeyboard {
 	@Shadow @Final private MinecraftClient client;
 
-	@Inject(method = "onKey", at = @At(value = "FIELD", ordinal = 0, target = "Lnet/minecraft/client/option/GameOptions;hudHidden:Z"))
+	@Inject(method = "onKey", at = @At(value = "FIELD", ordinal = 1, shift = Shift.AFTER, target = "Lnet/minecraft/client/option/GameOptions;hudHidden:Z"))
 	private void onToggleHud(long window, int key, int scancode, int action, int modifiers, CallbackInfo ci) {
-		((ClientAccessor) client).updateFpsString();
+		if(client.getDebugHud().shouldShowDebugHud()) {
+			((ClientAccessor) client).badoptimizations$updateFpsString();
+		}
 	}
 }
