e010e8623fb629debd3cb10f9b055a32758c6e1e
Thosea <104597976+ItsThosea@users.noreply.github.com>
2023-12-23 23:57:02 -0500

    1.3.1

--- src/main/java/me/thosea/badoptimizations/mixin/MixinKeyboard.java
+++ src/main/java/me/thosea/badoptimizations/mixin/MixinKeyboard.java
@@ -7,6 +7,7 @@ import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.At.Shift;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
@@ -14,10 +15,17 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public final class MixinKeyboard {
 	@Shadow @Final private MinecraftClient client;
 
-	@Inject(method = "onKey", at = @At(value = "FIELD", ordinal = 2, target = "Lnet/minecraft/client/option/GameOptions;debugEnabled:Z"))
-	private void renderInject(CallbackInfo ci) {
-		if(client.options.debugEnabled) {
-			((ClientAccessor) client).updateFpsString();
+	@Inject(method = "onKey", at = @At(value = "FIELD", shift = Shift.AFTER, ordinal = 1, target = "Lnet/minecraft/client/option/GameOptions;debugEnabled:Z"))
+	private void onToggleF3(CallbackInfo ci) {
+		if(client.options.debugEnabled && !client.options.hudHidden) {
+			((ClientAccessor) client).badoptimizations$updateFpsString();
+		}
+	}
+
+	@Inject(method = "onKey", at = @At(value = "FIELD", ordinal = 1, shift = Shift.AFTER, target = "Lnet/minecraft/client/option/GameOptions;hudHidden:Z"))
+	private void onToggleHud(long window, int key, int scancode, int action, int modifiers, CallbackInfo ci) {
+		if(client.options.debugEnabled && !client.options.hudHidden) {
+			((ClientAccessor) client).badoptimizations$updateFpsString();
 		}
 	}
 }
