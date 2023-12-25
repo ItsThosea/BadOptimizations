2ac8071cff47b48192d7f44d6c5f57012e02fcf6
Thosea <104597976+ItsThosea@users.noreply.github.com>
2023-12-24 23:37:33 -0500

    1.4.0

--- src/main/java/me/thosea/badoptimizations/mixin/MixinKeyboard.java
+++ src/main/java/me/thosea/badoptimizations/mixin/MixinKeyboard.java
@@ -17,14 +17,14 @@ public final class MixinKeyboard {
 
 	@Inject(method = "onKey", at = @At(value = "FIELD", shift = Shift.AFTER, ordinal = 1, target = "Lnet/minecraft/client/option/GameOptions;debugEnabled:Z"))
 	private void onToggleF3(CallbackInfo ci) {
-		if(client.options.debugEnabled && !client.options.hudHidden) {
+		if(ClientAccessor.shouldUpdateFpsString(client)) {
 			((ClientAccessor) client).badoptimizations$updateFpsString();
 		}
 	}
 
 	@Inject(method = "onKey", at = @At(value = "FIELD", ordinal = 1, shift = Shift.AFTER, target = "Lnet/minecraft/client/option/GameOptions;hudHidden:Z"))
 	private void onToggleHud(long window, int key, int scancode, int action, int modifiers, CallbackInfo ci) {
-		if(client.options.debugEnabled && !client.options.hudHidden) {
+		if(ClientAccessor.shouldUpdateFpsString(client)) {
 			((ClientAccessor) client).badoptimizations$updateFpsString();
 		}
 	}
