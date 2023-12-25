e0c5477b6678a80a896fbb573c1eb2e11a2ba314
Thosea <104597976+ItsThosea@users.noreply.github.com>
2023-12-24 23:37:33 -0500

    1.4.0

--- src/main/java/me/thosea/badoptimizations/ClientAccessor.java
+++ src/main/java/me/thosea/badoptimizations/ClientAccessor.java
@@ -1,5 +1,11 @@
 package me.thosea.badoptimizations;
 
+import net.minecraft.client.MinecraftClient;
+
 public interface ClientAccessor {
 	void badoptimizations$updateFpsString();
+
+	static boolean shouldUpdateFpsString(MinecraftClient client) {
+		return client.options.debugEnabled && (!client.options.hudHidden || client.currentScreen != null);
+	}
 }
