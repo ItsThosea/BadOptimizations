f24260e40b6c3342d805e75a7eb3518640a91510
Thosea <104597976+ItsThosea@users.noreply.github.com>
2024-01-05 22:07:17 -0500

    1.6.0
    (cherry picked from commit e57167467280f5c067529bab0a2fa30f65e21c38)

---
+++ src/main/java/me/thosea/badoptimizations/mixin/tick/MixinClientWorldSkyColor.java
@@ -0,0 +1,105 @@
+package me.thosea.badoptimizations.mixin.tick;
+
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.world.ClientWorld;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.profiler.Profiler;
+import net.minecraft.util.registry.RegistryEntry;
+import net.minecraft.util.registry.RegistryKey;
+import net.minecraft.world.MutableWorldProperties;
+import net.minecraft.world.World;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.dimension.DimensionType;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.function.Supplier;
+
+@Mixin(ClientWorld.class)
+public abstract class MixinClientWorldSkyColor extends World {
+	@Shadow @Final private MinecraftClient client;
+
+	@Unique private Vec3d skyColorCache;
+	@Unique private int lastTick;
+
+	@Unique private long previousTime;
+	@Unique private RegistryEntry<Biome> previousBiome;
+	@Unique private float previousRainGradient;
+	@Unique private float previousThunderGradient;
+	@Unique private int previousLightningTicks;
+
+	@Unique
+	private boolean isSkyColorDirty(Vec3d pos) {
+		boolean result = false;
+
+		long time = getTimeOfDay(); // public World method
+		if(Math.abs(time - previousTime) >= 50) {
+			previousTime = time;
+			result = true;
+		}
+
+		RegistryEntry<Biome> biome = getBiomeAccess().getBiomeForNoiseGen(pos.x, pos.y, pos.z);
+		if(previousBiome != biome) {
+			previousBiome = biome;
+			result = true;
+		}
+
+		float rainGradient = this.rainGradient; // protected in World
+		if(previousRainGradient != rainGradient) {
+			previousRainGradient = rainGradient;
+			result = true;
+		}
+
+		float thunderGradient = this.thunderGradient; // protected in World
+		if(previousThunderGradient != thunderGradient) {
+			previousThunderGradient = thunderGradient;
+			result = true;
+		}
+
+		int lightningTicks = getLightningTicksLeft(); // shadow method
+		if(previousLightningTicks != lightningTicks) {
+			previousLightningTicks = lightningTicks;
+			result = true;
+		}
+
+		return result;
+	}
+
+	@Shadow public abstract int getLightningTicksLeft();
+
+	@Inject(method = "getSkyColor", at = @At("HEAD"), cancellable = true)
+	private void onGetColorHead(Vec3d cameraPos, float tickDelta, CallbackInfoReturnable<Vec3d> cir) {
+		int tick = client.player.age;
+
+		if(lastTick != tick) {
+			lastTick = tick;
+
+			if(isSkyColorDirty(cameraPos)) {
+				return;
+			}
+		}
+
+		cir.setReturnValue(skyColorCache);
+	}
+
+	@Inject(method = "getSkyColor", at = @At("TAIL"))
+	private void onGetColorTail(Vec3d cameraPos, float tickDelta, CallbackInfoReturnable<Vec3d> cir) {
+		skyColorCache = cir.getReturnValue();
+	}
+
+	@Inject(method = "<init>", at = @At("TAIL"))
+	private void afterInit(CallbackInfo ci) {
+		lastTick = -1;
+	}
+
+	protected MixinClientWorldSkyColor(MutableWorldProperties properties, RegistryKey<World> registryRef, RegistryEntry<DimensionType> dimension, Supplier<Profiler> profiler, boolean isClient, boolean debugWorld, long seed, int maxChainedNeighborUpdates) {
+		super(properties, registryRef, dimension, profiler, isClient, debugWorld, seed, maxChainedNeighborUpdates);
+		throw new AssertionError("nuh uh");
+	}
+}
