72fb2c4e26d9c879f4b997b365eae5128a8c6faf
Thosea <104597976+ItsThosea@users.noreply.github.com>
2024-01-07 22:06:45 -0500

    1.6.1

--- common/src/main/java/me/thosea/badoptimizations/mixin/MixinDataTracker.java
+++ common/src/main/java/me/thosea/badoptimizations/mixin/MixinDataTracker.java
@@ -15,14 +15,9 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.At.Shift;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
-import java.util.Iterator;
-import java.util.List;
 import java.util.concurrent.locks.ReadWriteLock;
 
-// All entity data is only accessed on one thread,
-// so this mixin removes unnecessary locks.
 @Mixin(DataTracker.class)
 public abstract class MixinDataTracker {
 	@Shadow @Final @Mutable private ReadWriteLock lock;
@@ -39,10 +34,8 @@ public abstract class MixinDataTracker {
 		entityAccessor.badoptimizations$refreshEntityData(key);
 	}
 
-	@Inject(method = "writeUpdatedEntries", locals = LocalCapture.CAPTURE_FAILHARD, at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/Entity;onTrackedDataSet(Lnet/minecraft/entity/data/TrackedData;)V"))
-	private void onBulkDataSet(List<Entry<?>> entries, CallbackInfo ci,
-	                           Iterator<?> var2,
-	                           Entry<?> entry, Entry<?> entry2) {
-		entityAccessor.badoptimizations$refreshEntityData(entry.getData());
+	@Inject(method = "copyToFrom", at = @At("TAIL"))
+	private void onCopy(Entry<?> to, Entry<?> from, CallbackInfo ci) {
+		entityAccessor.badoptimizations$refreshEntityData(to.getData());
 	}
 }
