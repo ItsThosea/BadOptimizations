8a3a734e869bb3aec6738d3e7d854a5723084587
Thosea <104597976+ItsThosea@users.noreply.github.com>
2024-02-14 22:16:17 -0500

    2.0.1

---
+++ common/src/main/java/me/thosea/badoptimizations/interfaces/LightmapMethods.java
@@ -0,0 +1,7 @@
+package me.thosea.badoptimizations.interfaces;
+
+import me.thosea.badoptimizations.mixin.accessor.GameRendererAccessor;
+
+public interface LightmapMethods {
+	void bo$setGameRendererAccessor(GameRendererAccessor accessor);
+}
--- common/src/main/java/me/thosea/badoptimizations/mixin/MixinWorldRenderer.java
+++ common/src/main/java/me/thosea/badoptimizations/mixin/MixinWorldRenderer.java
@@ -10,7 +10,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Mixin(WorldRenderer.class)
+@Mixin(value = WorldRenderer.class, priority = 700)
 public abstract class MixinWorldRenderer {
 	@WrapOperation(method = "renderSky(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/util/math/Matrix4f;FLnet/minecraft/client/render/Camera;ZLjava/lang/Runnable;)V",
 			at = @At(value = "INVOKE", target = "Lnet/minecraft/client/world/ClientWorld;getSkyAngle(F)F", ordinal = 0))
