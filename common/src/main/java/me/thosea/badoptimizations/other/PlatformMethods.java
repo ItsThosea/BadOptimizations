e6b1ca6292f543a39654e75d1b36abfbdd82bcf6
Thosea <104597976+ItsThosea@users.noreply.github.com>
2024-02-26 03:30:30 -0500

    2.1.0

---
+++ common/src/main/java/me/thosea/badoptimizations/other/PlatformMethods.java
@@ -0,0 +1,19 @@
+package me.thosea.badoptimizations.other;
+
+import dev.architectury.injectables.annotations.ExpectPlatform;
+
+import java.io.File;
+
+public final class PlatformMethods {
+	private PlatformMethods() {}
+
+	@ExpectPlatform
+	public static String getVersion() {
+		throw new AssertionError();
+	}
+
+	@ExpectPlatform
+	public static File getConfigFolder() {
+		throw new AssertionError();
+	}
+}
