c287839d4902d4a5f97cdc2a2b0319659e3217b0
Thosea <104597976+ItsThosea@users.noreply.github.com>
2024-02-26 03:30:30 -0500

    2.1.0

--- common/src/main/java/me/thosea/badoptimizations/mixin/MixinDebugHud_AddText.java
+++ common/src/main/java/me/thosea/badoptimizations/mixin/MixinDebugHud_AddText.java
@@ -1,19 +1,23 @@
 package me.thosea.badoptimizations.mixin;
 
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
-import me.thosea.badoptimizations.other.VersionSupplier;
+import me.thosea.badoptimizations.other.PlatformMethods;
 import net.minecraft.client.gui.hud.DebugHud;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 
 import java.util.List;
 
-@Mixin(DebugHud.class)
-public abstract class MixinDebugHud {
+@Mixin(value = DebugHud.class, priority = 999)
+public class MixinDebugHud_AddText {
+	@Unique
+	private static final String F3_TEXT = "BadOptimizations " + PlatformMethods.getVersion();
+
 	@ModifyReturnValue(method = "getLeftText", at = @At("RETURN"))
 	private List<String> addBadOptimizationsText(List<String> list) {
 		list.add("");
-		list.add(VersionSupplier.F3_TEXT);
+		list.add(F3_TEXT);
 		return list;
 	}
 }
